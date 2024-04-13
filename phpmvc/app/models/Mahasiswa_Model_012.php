98ac8795bb995d43dd9e904aa407943c1b28f999
jeyshen siallagan <151107503+Jeyshen@users.noreply.github.com>
2024-04-13 21:15:20 +0700

    Add files via upload

---
+++ phpmvc/app/models/Mahasiswa_Model_012.php
@@ -0,0 +1,103 @@
+<?php
+
+class Mahasiswa_model {
+    private $table = 'mahasiswa';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    // private $mhs = [
+    //     [
+    //         "nama" => "Jeyshen Siallagan",
+    //         "nrp" => "13323012",
+    //         "email" => "jeyshensiallagan8@gmail.com",
+    //         "jurusan" => "D3 Teknologi Komputer"
+    //     ],
+    //     [
+    //         "nama" => "Yehezkiel Rumapea",
+    //         "nrp" => "13323032",
+    //         "email" => "yehezkiel12345678@gmail.com",
+    //         "jurusan" => "D3 Teknologi Komputer"
+    //     ],
+    //     [
+    //     "nama" => "Rudi Tabuti",
+    //     "nrp" => "13323060",
+    //     "email" => "rudyganteng2345678@gmail.com",
+    //     "jurusan" => "D3 Teknologi Komputer"
+    //     ]
+    //     ];
+
+        public function getAllMahasiswa()
+        {
+           $this->db->query('SELECT * FROM ' . $this->table);
+           return $this->db->resultSet();
+        }
+
+        public function getMahasiswaById($id)
+        {
+            $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+            $this->db->bind('id', $id);
+            return $this->db->single();
+        }
+
+        public function tambahDataMahasiswa($data) {
+            $query = "INSERT INTO mahasiswa
+                        VALUES
+                        ('', :nama, :nrp, :email, :jurusan)";
+
+            $this->db->query($query);
+            $this->db->bind('nama', $data['nama']);
+            $this->db->bind('nrp', $data['nrp']);
+            $this->db->bind('email', $data['email']);
+            $this->db->bind('jurusan', $data['jurusan']);
+
+            $this->db->execute();
+
+            return  $this->db->rowCount();
+            
+        }
+
+        public function hapusDataMahasiswa($id)
+        {
+            $query = "DELETE FROM mahasiswa WHERE id = :id";
+            $this->db->query($query);
+            $this->db->bind('id', $id);
+
+            $this->db->execute();
+
+            return $this->db->rowCount();
+        }
+
+        public function ubahDataMahasiswa($data) {
+            $query = "UPDATE mahasiswa SET
+                        nama = :nama,
+                        nrp = :nrp,
+                        email = :email,
+                        jurusan = :jurusan
+                        WHERE id= :id";
+        
+            $this->db->query($query);
+            $this->db->bind('nama', $data['nama']);
+            $this->db->bind('nrp', $data['nrp']);
+            $this->db->bind('email', $data['email']);
+            $this->db->bind('jurusan', $data['jurusan']);
+            $this->db->bind('id', $data['id']);
+        
+            $this->db->execute();
+        
+            return  $this->db->rowCount();            
+        }
+
+        public function cariDataMahasiswa()
+        {
+            $keyword = $_POST['keyword'];
+            $query = "SELECT * FROM mahasiswa WHERE nama LIKE :keyword";
+            $this->db->query($query);
+            $this->db->bind('keyword', "%$keyword%");
+            return $this->db->resultSet();
+        }
+        
+
+}
